76d18db90b27695f18a2e5ea46cf5765269a9fd4
Jordy_HOME <jordyjordy@naver.com>
2024-09-23 23:47:25 +0900

    좀더 공부

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,8 @@
 ﻿
 #include <iostream>
 
+
+
 int main()
 {
 	int Arr[5] = {1, 10, 100, 1000};
@@ -8,18 +10,30 @@ int main()
 	int ArrTest1 = Arr[1];
 	int ArrTest2 = Arr[2];
 
-	const char* const Text = "ABCD";
-	char Text0 = Text[0];
-	char Text1 = Text[1];
-	char Text2 = Text[2];
-	char Text3 = Text[3];
-	char Text4 = Text[4];
-	char Text5 = Text[5];
+	const char* Text0 = "ABCD";
+	char Text0_0 = Text0[0];
+	char Text0_1 = Text0[1];
+	char Text0_2 = Text0[2];
+	char Text0_3 = Text0[3];
 
+	const char* const Text1 = "QWER";
+	char Text1_0 = Text1[0];
+	char Text1_1 = Text1[1];
+	char Text1_2 = Text1[2];
+	char Text1_3 = Text1[3];
+
+					  // 배열 이름이 주소가 될수 있다..
+	const char* const Text2[4] = { "A","S","D","F" };
+	char Text2_0 = *Text2[0];
+	char Text2_1 = *Text2[1];
+	char Text2_2 = *Text2[2];
+	char Text2_3 = *Text2[3];
+			 
 	int PtrArr[5] = {10, 20, 30, 40, 50};
 	int* Ptr = PtrArr;
 	int AAA = Ptr[0];
 	int BBB = Ptr[1];
+	Ptr[3] = 100;
 
 
 
