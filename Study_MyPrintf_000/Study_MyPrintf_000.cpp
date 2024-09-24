b7d1e23e3906df99f5c49693ff95a8a9671c8f91
Jordy_HOME <jordyjordy@naver.com>
2024-09-25 01:59:06 +0900

    정리

--- Study_MyPrintf_000/Study_MyPrintf_000.cpp
+++ Study_MyPrintf_000/Study_MyPrintf_000.cpp
@@ -16,7 +16,7 @@ int NumberCount(int _Value)
 }
 
 //	 NumberToString(Arr[10],      IntMaxCount(==10), ConvertValue);
-void NumberToString(char* Buffer, int BufferSize,    int _Value)
+void NumberToString(char* Buffer, int BufferSize, int _Value)
 {
 	int Count = NumberCount(_Value);
 
@@ -44,8 +44,8 @@ int MyPrintf(const char* const _Ptr, ...)
 	// 첫번째 인자 _Ptr의 주소를 __int64형 FAdd에 대입하면서 초기화
 	// &_Ptr 주소를 정수 __int64로 강제 캐스팅 하는 이유
 	// 첫번째 인자인 _Ptr의 주소를 정확하게 받기 위하여
-	__int64 FAdd = reinterpret_cast<__int64>(&_Ptr); 
-	
+	__int64 FAdd = reinterpret_cast<__int64>(&_Ptr);
+
 	// int형 변수 ChCount 선언 및 초기화 
 	int ChCount = 0;
 
@@ -69,25 +69,25 @@ int MyPrintf(const char* const _Ptr, ...)
 
 			switch (Ch)
 			{
-			// & 다음에 오는 문자가 d인 경우
+				// & 다음에 오는 문자가 d인 경우
 			case 'd':
 			{
-			// 포인터 Ptr 선언
-			// FAdd 값(== 첫번째 인자의 주소)을 강제 포인터 캐스팅
-			// 이는 즉 첫번째 인자 주소에 접근하겠다는 의미
-			// 그리고 ParameterInter 값(==8) 만큼 이동 후 다시 대입
-				// 8을 더하는 이유 == 다음 인자로 이동하기 위해!
-			int* Ptr = reinterpret_cast<int*>(FAdd += ParameterInter);
-			// 다음 인자에 있는 값을 ConvertValue에 대입
-			int ConvertValue = *Ptr;
-			// 문자열 Arr 선언
-			// IntMaxCount == 10
-			// 즉 Arr[10] == 메모리 공간 10개를 붙여서 준비하라는 뜻
-			char Arr[IntMaxCount] = {};
-			// NumberToString 함수 호출
-			// 차례로 Arr, 10, ConvertValue 넣음
-			// 숫자를 문자열로 바꾸는 함수
-			NumberToString(Arr, IntMaxCount, ConvertValue);
+				// 포인터 Ptr 선언
+				// FAdd 값(== 첫번째 인자의 주소)을 강제 포인터 캐스팅
+				// 이는 즉 첫번째 인자 주소에 접근하겠다는 의미
+				// 그리고 ParameterInter 값(==8) 만큼 이동 후 다시 대입
+					// 8을 더하는 이유 == 다음 인자로 이동하기 위해!
+				int* Ptr = reinterpret_cast<int*>(FAdd += ParameterInter);
+				// 다음 인자에 있는 값을 ConvertValue에 대입
+				int ConvertValue = *Ptr;
+				// 문자열 Arr 선언
+				// IntMaxCount == 10
+				// 즉 Arr[10] == 메모리 공간 10개를 붙여서 준비하라는 뜻
+				char Arr[IntMaxCount] = {};
+				// NumberToString 함수 호출
+				// 차례로 Arr, 10, ConvertValue 넣음
+				// 숫자를 문자열로 바꾸는 함수
+				NumberToString(Arr, IntMaxCount, ConvertValue);
 
 
 
@@ -109,7 +109,7 @@ int MyPrintf(const char* const _Ptr, ...)
 
 
 	}
-	
+
 	return 0;
 }
 
@@ -120,7 +120,7 @@ int main()
 {
 
 	MyPrintf("Number : %d\n", 545454);
-	
+
 	// 문자열 자체가 주소처럼 된다
 	// 때문에 [2]를 쓰면 세번째에 있는 C가 test에 대입된다.
 	char test = "ABCD"[2];
