1a1963536d9475470d889aa610d8d50407cf3ac7
Jordy Jordy <rinnn1212@gmail.com>
2024-09-27 18:16:18 +0900

    클래스 사이즈 확인

---
+++ 004_ClassSize/004_ClassSize.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+
+class Test
+{
+public:
+	Test()
+	{
+
+	}
+
+	int TestValue = 10;
+
+protected:
+private:
+};
+
+
+
+int main()
+{
+	int ClassSize = sizeof(Test);
+
+	Test NewTest = Test();
+
+	__int64 Address = reinterpret_cast<__int64>(&NewTest);
+	int* ClassPtr = &NewTest.TestValue;
+
+	
+	int a = 0;
+
+
+}
