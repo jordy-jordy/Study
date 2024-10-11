855bdff082c24c998d188440a7a5c9f890d5c1cd
Jordy Jordy <rinnn1212@gmail.com>
2024-10-11 11:50:00 +0900

    이현정 : 2차원 배열 삭제

---
+++ 070_NewTest/070_NewTest.cpp
@@ -0,0 +1,34 @@
+﻿// 070_NewTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    "&&**";
+    "&&**";
+    "****";
+    "****";
+
+    int ImageScaleY = 4;
+    char** AllImagePixel = new char* [ImageScaleY] {0, };
+    char* Arr[4];
+
+    int ImageScaleX = 4;
+    AllImagePixel[0] = new char[ImageScaleX + 1];
+    AllImagePixel[1] = new char[ImageScaleX + 1];
+    AllImagePixel[2] = new char[ImageScaleX + 1];
+    AllImagePixel[3] = new char[ImageScaleX + 1];
+
+    if (nullptr != AllImagePixel)
+    {
+        delete[] AllImagePixel[0];
+        delete[] AllImagePixel[1];
+        delete[] AllImagePixel[2];
+        delete[] AllImagePixel[3];
+        delete[] AllImagePixel;
+        AllImagePixel = nullptr;
+    }
+}
